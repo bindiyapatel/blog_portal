8ee427c6e74021f998182ebd41009565f7048d07
bindiya patel <bindiyapatel7069@gmail.com>
2021-03-03 17:08:24 +0530

    header and footer created. cdn files loaded

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div class="text-center p-3 footer" style="background-color: pink;">
+    © 2021 Copyright:
+    <a class="text-dark" href="http://localhost/php_course_project/">Online Blog Portal</a>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title> Bindiya Blogs</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bootstrap.min.css">
+        <script src="jquery.min.js"></script>
+        <script src="bootstrap.min.js"></script>
+        <script src="popper.min.js"></script>
+
+        <style>
+        .footer {
+          position: fixed;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          background-color: red;
+          color: white;
+          text-align: center;
+        }
+        </style>      
+    </head>
+    <body>
+
+    
+  <div class="text-center bg-pink">
+    <h1>Online Blog Portal</h1>
+    <p>Express your ideas and thoughts here.</p> 
+  </div>
