841ec8988b42a743a9575fcdf4b82930ca84c837
bindiya patel <bindiyapatel7069@gmail.com>
2021-03-03 17:12:39 +0530

    list of blog page added

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    include_once "header.php";
+?>
+
+<?php
+    // List of Blog Display
+    include_once "listBlog.php";
+?>
+
+<?php
+    include_once "footer.php"
+?>
---
+++ listBlog.php
@@ -0,0 +1,31 @@
+
+
+<div class="alert alert-dismissible" id="responseAlert" style="display:none;">
+        <!-- <strong>Success!</strong> Indicates a successful or positive action. -->
+</div>
+    
+<div class="container-fluid" id="blogForm">
+  
+  <div class="card bg-light text-dark blogDiv p-1">
+    <div class="card-body p-1 pull-right">
+      <a href="addBlog.php" class="btn btn-success mr-1">New Blog</a>
+    </div>
+  </div>
+  
+  <input type="hidden" id="blog_id" value="all" />
+  <div>
+    <div id="listBlogs" style="overflowy: auto; max-height: 400px; margin-top:50px">
+    </div>
+  </div>
+  
+</div>
+
+<div>
+    <span class="btn" id="view_more">View more</span>
+  </div>
+
+<div id="loader">
+    Loading...
+</div>
+<script src="blog.js"></script>
+
